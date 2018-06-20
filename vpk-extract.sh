c963760956e9ca505b59d8e07169da49076ac32b
4parthy <aparthy@zoho.com>
2018-06-20 04:36:38 +0200

    fixed extract script

--- vpk-extract.sh
+++ vpk-extract.sh
@@ -4,6 +4,6 @@ vpk l $vpkfile > files.txt
 while read -r line
 do
     path=${line%/*}
-    if [ -d "$path" ]; then mkdir -p $path; fi
+    if [ ! -d "$path" ]; then mkdir -p $path; fi
     vpk -v x $vpkfile $line
 done < files.txt
